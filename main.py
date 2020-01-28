23610ca318661e294dddd401671da1a70ccd696d
Dalynn Hatch <dalynn.hatch@outlook.com>
2020-01-28 13:23:29 -0600

    setting up main if elif statements

--- main.py
+++ main.py
@@ -8,3 +8,24 @@ print('7) Add record')
 print('8) Delete record')
 print('9) Quit')
 choice = input()
+
+if choice == '1':
+    print('Create db')
+elif choice == '2':
+    print('Open db')
+elif choice == '3':
+    print('Close db')
+elif choice == '4':
+    print('Display record')
+elif choice == '5':
+    print('Update record')
+elif choice == '6':
+    print('Create report')
+elif choice == '7':
+    print('Add record')
+elif choice == '8':
+    print('Delete record')
+elif choice == '9':
+    print('Quitting...')
+else:
+    print('Choice not valid')
